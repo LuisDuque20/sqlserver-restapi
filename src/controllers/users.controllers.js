7ac5afc660485268d0431bde5f36304fac5f9731
robnez44 <00044623@uca.edu.sv>
2025-06-03 19:12:18 -0600

    Peticiono de rutas favoritas por usuario

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,3 +1,4 @@
+import sql from 'mssql'
 import { getConnection } from "../database/connection.js"
 
 export const getUsers = async (req, res) => {
@@ -7,6 +8,34 @@ export const getUsers = async (req, res) => {
         res.json(result.recordset)
     } catch (error) {
         console.error("Error en getUsers:", error)
-        res.status(500).json({ message: "Error al obtener los usuarios", error: error.message })
+        res.status(500).json({ message: "Error al obtener los rutasusuarios", error: error.message })
+    }
+}
+
+export const getFavoritas = async (req, res) => {
+    try {
+        const idUsuario = parseInt(req.params.id);
+        if (isNaN(id)) return res.status(400).json({ message: "ID de usuario inválido" });
+
+        const pool = await getConnection()
+        const ps = new sql.PreparedStatement(pool);
+        ps.input('usuarioId', sql.Int);
+
+        await ps.prepare(`
+        SELECT A.*
+        FROM Usuario_AutobusFavorito UAF
+        JOIN Autobus A ON A.Id = UAF.AutobusId
+        WHERE UAF.UsuarioId = @usuarioId
+        `);
+        const result = await ps.execute({ usuarioId });
+
+        if (result.recordset.length === 0) {
+            return res.status(404).json({ message: "Este usuario no tiene rutas favoritas." });
+        } else { res.json({ message: `Ruta actualizada correctamente correctamente` }) }
+    } catch (err) {
+        console.error("Error al mostrar las rutas favoritas del usuario:", err);
+        res.status(500).json({ message: "Error del servidor" });
+    } finally {
+        if (ps) await ps.unprepare();
     }
 }
--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,8 +1,13 @@
 import {Router} from 'express'
 import { getUsers } from '../controllers/users.controllers.js'
+import { getFavoritas } from '../controllers/users.controllers.js'
 
 const router = Router()
 
+// Obtenes todos los usuarios
 router.get('/usuario',getUsers)
+
+// Mostrar rutas favoritas por usuario
+router.get('/usuario/:id/favorita', getFavoritas)
 
 export default router
