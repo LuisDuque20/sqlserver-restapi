3d0d0146b9b2970734bb135609d5ed81ecbfaca6
LuisDuque20 <00013423@uca.edu.sv>
2025-06-13 19:54:23 -0600

    Cambiando parametros de conexion

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,6 +4,7 @@ const dbSettings = {
     user: `${process.env.DB_USER}`,
     password: `${process.env.DB_PASSWORD}`,
     server: `${process.env.DB_HOST}`,
+    port:Number(process.env.PORT),
     database: `${process.env.DB_NAME}`,
     options: {
         encrypt: false,
